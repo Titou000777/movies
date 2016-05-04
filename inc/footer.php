a69b0d41793b973982299a7f90e3158364517f85
ChristineCom <chcomte27@gmail.com>
2016-05-04 14:05:33 +0200

    premier commit structure HTML

---
+++ inc/footer.php
@@ -0,0 +1,7 @@
+<footer>
+	<p>Pied de page</p>
+</footer>
+
+</body>
+
+</html>
---
+++ inc/header.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+	<meta charset="utf-8" />
+	<title>Ciné Movie</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.1/css/font-awesome.min.css">
+	<link href='https://fonts.googleapis.com/css?family=Roboto+Slab:400,700,300,100' rel='stylesheet' type='text/css'>
+	<link rel="stylesheet" type="text/css" href="assets/css/style.css" />
+	<script src="assets/js/app.js"></script>
+	<script src="assets/js/jquery.min.js"></script>
+</head>
+
+<header>
+<!---  LOGO -->
+	<div id="">
+		<img src="assets/img/logo.png" alt="logo" class="" />
+		<h1>Ciné Movies</h1>
+	</div>
+
+	<!-- LIENS PAGES FILMS A VOIR ET FILMS VUS -->
+	<nav>
+		<ul>
+			<li><a href="#">Films à voir</a></li>
+			<li><a href="#">Films vus</a></li>
+		</ul>
+	</nav>
+
+	<ul id="">
+		<li><a href="#">Inscription</a></li>
+		<li><a href="#">Connexion</a></li>
+		<li><a href="#">Déconnexion</a></li>
+	</ul>
+	</header>
+
+<body>
+
+
