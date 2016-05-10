9fe68333c715d70a54a655671d53abd7a3a7965c
Cédric <sophie.drazic@gmail.com>
2016-05-10 14:59:39 +0200

    routes

--- user/dashboard.php
+++ user/dashboard.php
@@ -10,12 +10,31 @@
 	// if(empty($_SESSION['user'])){
 	// 	header('Location: login.php');
 	// }
+	$page = false;
+
+	// On vérifie sur quel page on est :
+	if(isset($_GET['cat']) && $_GET['cat'] == "tosee") {
+		$title = 'Films à voir';
+		$page = true;
+	}
+	else if(isset($_GET['cat']) && $_GET['cat'] == "seen") {
+		$title = 'Films vus';
+		$page = true;
+	}
+	else {
+		$title = "Bonjour";
+	}
+
+	// Si l'on est pas sur la page d'accueil on vérifie les cookie :
+	if(!empty($_COOKIE['tosee'])) {
+		debug($_COOKIE['tosee']);
+	}
 
 ?>
 <?php include('../inc/header.php'); ?>
 
 <section class="wrapper">
-	<h2>Bonjour <?= $_SESSION['user']['pseudo'] ?> bienvenu dans votre espace personnel</h2>
+	<h2><?= $title ?></h2>
 </section>
 
 <?php include('../inc/footer.php'); ?>
